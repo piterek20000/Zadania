7d33b584d36fb13cbd3d2222ef7060d596ee4d1b
piterek20000 <31956507+piterek20000@users.noreply.github.com>
2017-09-19 16:48:07 +0200

    Add files via upload

---
+++ Proste dodawanie/Proste dodawanie/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+public class Test
+{
+    public static void Main()
+    {
+        int testy;
+
+        testy = Int32.Parse(Console.ReadLine());
+
+        int[] wyniki = new int[testy];
+
+        for (int i = 0; i < testy; i++)
+        {
+            int suma = 0;
+
+            int cyfr;
+
+            cyfr = Int32.Parse(Console.ReadLine());
+
+            string pomocna = Console.ReadLine();
+
+            string[] wierszLiczb = pomocna.Split(' ');
+
+            foreach (var item in wierszLiczb)
+            {
+                suma += Int32.Parse(item);
+            }
+
+            wyniki[i] = suma;
+        }
+
+        foreach (var item in wyniki)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}

122792cc0f07111542b39f0f463049dffb30018b
piterek20000 <31956507+piterek20000@users.noreply.github.com>
2017-09-19 15:25:02 +0200

    Add files via upload

---
+++ Formatowanie wyrazow/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mazak_z_Spoj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Word = Console.ReadLine();
+            string New_Word = "";
+
+            int Count = 1;
+
+            for (int i = 1; i < Word.Length; i++)
+            {
+                if (Word[i-1].Equals(Word[i]))
+                {
+                    Count++;
+                    if (i == Word.Length-1)
+                    {
+                        if (Count > 2)
+                        {
+                            New_Word += string.Format("{0}{1}", Word[i - 1], Count);
+                            Count = 1;
+                        }
+                        else if (Count == 2)
+                        {
+                            New_Word += string.Format("{0}{0}", Word[i - 1]);
+                            Count = 1;
+                        }
+                        else
+                        {
+                            New_Word += Word[i - 1];
+                        }
+                    }
+                }
+
+                else
+                {
+                    if (Count>2)
+                    {
+                        New_Word += string.Format("{0}{1}", Word[i - 1], Count);
+                        Count = 1;
+                    }
+                    else if (Count == 2)
+                    {
+                        New_Word += string.Format("{0}{0}",Word[i - 1]);
+                        Count = 1;
+                    }
+                    else
+                    {
+                        New_Word += Word[i - 1];
+                    }
+                }
+            }
+            Console.WriteLine(New_Word);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Zliczanie liter w zdaniu/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zliczanie_Liter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string zdanie = Console.ReadLine();
+
+            //Tablica z szukanymi literami
+            char[] Tablica_Liter =
+            {
+                 'a','b','c','d','e','f','g','h',
+                 'i','j','k','l','m','n','o','p',
+                 'q','r','s','t','u','v','w','x',
+                 'y','z',
+                 'A','B','C','D','E','F','G','H',
+                 'I','J','K','L','M','N','O','P',
+                 'Q','R','S','T','U','V','W','X',
+                 'Y','Z'
+            };
+
+            //Tablica odpowiadająca ilości wystąpien poszczegulnych liter
+            int[] Tablica_Wartosci = new int[Tablica_Liter.Length];
+
+            Ile_Liter(zdanie, Tablica_Liter, Tablica_Wartosci);
+
+            Ile_Razy(Tablica_Liter, Tablica_Wartosci);
+
+            Console.ReadLine();
+
+        }
+
+
+        /// <summary>
+        /// Metoda wyświetlająca ile razy wystąpiła poszczegulna litera
+        /// </summary>
+        /// <param name="Tablica_Liter">Tablica szukanych liter</param>
+        /// <param name="Tablica_Wartosci">Tablica wystąpien danej litery</param>
+        private static void Ile_Razy(char[] Tablica_Liter, int[] Tablica_Wartosci)
+        {
+            for (int i = 0; i < Tablica_Liter.Length; i++)
+            {
+                if(Tablica_Wartosci[i] != 0)
+                Console.WriteLine("\n\t Litera: {0}, {1} razy.", Tablica_Liter[i], Tablica_Wartosci[i]);
+            }
+        }
+
+        /// <summary>
+        /// Metoda sprawdzająca ile razy wystąpił poszczegulny znak w zdaniu
+        /// </summary>
+        /// <param name="zdanie"></param>
+        /// <param name="Tablica_Liter"></param>
+        /// <param name="Tablica_Wartosci"></param>
+        private static void Ile_Liter(string zdanie, char[] Tablica_Liter, int[] Tablica_Wartosci)
+        {
+            for (int i = 0; i < zdanie.Length; i++)
+            {
+                for (int j = 0; j < Tablica_Liter.Length; j++)
+                {
+                    if (zdanie[i].Equals(Tablica_Liter[j])) Tablica_Wartosci[j] += 1;
+                }
+            }
+        }
+    }
+}

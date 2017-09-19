7af5cbd1b643308b5a5df6c1bcb4540a88b7b867
piterek20000 <31956507+piterek20000@users.noreply.github.com>
2017-09-19 17:01:18 +0200

    Add files via upload

---
+++ Pole pewnego kola/Pole pewnego koła/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pole_pewnego_koła
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double r, d;
+            double pi = 3.141592654;
+
+            string A = Console.ReadLine();
+            string[] B = A.Split(' ');
+
+            r = double.Parse(B[0]);
+            d = double.Parse(B[1]);
+
+            r *= r;
+            d *= d;
+
+            Console.WriteLine((r - d / 4) * pi);
+        }
+    }
+}

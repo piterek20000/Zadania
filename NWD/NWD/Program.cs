a36008d45e12231061dacf2bb1b6d2a319d7c057
piterek20000 <31956507+piterek20000@users.noreply.github.com>
2017-09-19 16:48:52 +0200

    Add files via upload

---
+++ NWD/NWD/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NWD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int testy;
+
+            testy = Int32.Parse(Console.ReadLine());
+            for (int i = 0; i < testy; i++)
+            {
+                int a, b;
+                string A = Console.ReadLine();
+                string[] B = A.Split(' ');
+
+                a = Int32.Parse(B[0]);
+                b = Int32.Parse(B[1]);
+
+                while (a != b)
+                {
+                    if (a > b)
+                        a -= b;
+                    else
+                        b -= a;
+                }
+
+                Console.WriteLine(a);
+            }
+            Console.ReadLine();
+        }
+    }
+}

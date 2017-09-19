bd5bc22bd56d3a747a75a3ea98ea8876afdbea56
piterek20000 <31956507+piterek20000@users.noreply.github.com>
2017-09-19 15:47:57 +0200

    Add files via upload

---
+++ Przedszkolanka/Przedszkolanka/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Przedszkolanka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1, n2;
+            int cukierki = 1;
+
+            int ile = Int32.Parse(Console.ReadLine());
+
+            for (int i = 0; i < ile; i++)
+            {
+                string pomocnicza = Console.ReadLine();
+                string[] pomocnicza1 = pomocnicza.Split(' ');
+                n1 = int.Parse(pomocnicza1[0]);
+                n2 = int.Parse(pomocnicza1[1]);
+
+                while (true)
+                {
+                    if ((cukierki % n1 == 0) && (cukierki % n2 == 0)) break;
+                    else
+                    {
+                        cukierki++;
+                    }
+                }
+
+                Console.WriteLine(cukierki);
+                cukierki = 1;
+            }
+        }
+    }
+}

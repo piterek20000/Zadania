41d4c10143e007d2d15939153129adcda86ac8c5
piterek20000 <31956507+piterek20000@users.noreply.github.com>
2017-09-20 19:58:26 +0200

    Add files via upload

---
+++ Predkość średnia/Predkość średnia/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Predkość_średnia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int testy = Int32.Parse(Console.ReadLine());
+
+            for (int i = 0; i < testy; i++)
+            {
+                string A = Console.ReadLine();
+                string[] B = A.Split(' ');
+
+                int V1 = Int32.Parse(B[0]);
+                int V2 = Int32.Parse(B[1]);
+
+                Console.WriteLine(2*V1*V2/(V1+V2));
+            }
+        }
+    }
+}

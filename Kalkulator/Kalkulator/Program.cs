4991b2f44b2c4bb042326e3eae1b61e1403965ba
piterek20000 <31956507+piterek20000@users.noreply.github.com>
2017-09-20 20:11:07 +0200

    Add files via upload

---
+++ Kalkulator/Kalkulator/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kalkulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool k = true;
+            while (k)
+            {
+                string A = Console.ReadLine();
+                string[] B = A.Split(' ');
+
+                int a = Int32.Parse(B[1]);
+                int b = Int32.Parse(B[2]);
+
+                switch (B[0])
+                {
+                    case "+":
+                        Console.WriteLine(a + b);
+                        break;
+                    case "-":
+                        Console.WriteLine(a - b);
+                        break;
+                    case "*":
+                        Console.WriteLine(a * b);
+                        break;
+                    case "/":
+                        Console.WriteLine(a / b);
+                        break;
+                    case "%":
+                        Console.WriteLine(a % b);
+                        break;
+
+                    default: k = false;
+                        break;
+                }
+
+            }
+        }
+    }
+}
